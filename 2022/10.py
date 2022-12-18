b212b0fee5057096219dc6d31e70f0ac3a13b3df
Tobias Chen <tobias@chen.de>
2022-12-18 20:36:22 +0100

    Add Days 10 for 2022 - fixed

--- 2022/10.py
+++ 2022/10.py
@@ -10,18 +10,20 @@ def readElements(file: str):
 class CPU:
     def __init__(self):
         self.register = 1
-        self.crtOutput = [["#"]]
+        self.crtOutput = [[]]
         self.clock = 0
         self.signalStrength = []
         self.relevantCycles = [40 + 40 * x for x in range(6)]
 
     def advanceClock(self):
         self.clock += 1
-        if self.clock in self.relevantCycles:
+        if self.clock - 1 in self.relevantCycles:
             self.crtOutput.append([])
             self.signalStrength.append((self.clock, self.register, self.clock * self.register))
-        if self.clock % 40 - reg
-
+        if -1 <= self.clock % 40 -1 - self.register <= 1:
+            self.crtOutput[-1].append("#")
+        else:
+            self.crtOutput[-1].append(".")
 
 def task1():
     instructions = readElements("10.txt")
@@ -39,33 +41,21 @@ def task1():
 
 
 def task2():
-    instructions = readElements("09.txt")
-    visitedPoints = []
-    headPosition = Point(0, 0)
-    tailPositions = []
-    for i in range(9):
-        tailPositions.append(Point(0, 0))
+    instructions = readElements("10.txt")
+    cpu = CPU()
     for instruction in instructions:
-        direction = instruction[0]
-        steps = int(instruction[1])
-        for step in range(steps):
-            if direction == "R":
-                headPosition.x += 1
-            elif direction == "L":
-                headPosition.x -= 1
-            elif direction == "U":
-                headPosition.y += 1
-            elif direction == "D":
-                headPosition.y -= 1
-            tailPositions[0] = tailFollowHead(tailPositions[0], headPosition)
-            for i in range(1, 9):
-                tailPositions[i] = tailFollowHead(tailPositions[i], tailPositions[i - 1])
-            visitedPoints.append(tailPositions[8])
+        if instruction[0] == "noop":
+            cpu.advanceClock()
+        elif instruction[0] == "addx":
+            cpu.advanceClock()
+            cpu.advanceClock()
+            cpu.register += int(instruction[1])
 
-    visitedPoints = list(set(visitedPoints))
-    visitedPoints.sort(key=lambda x: x.y)
-    print(
-        f" Task 2 | Total instructions: {len(instructions)}, Visited points: {visitedPoints} and {len(visitedPoints)}")
+
+    print(f" Task 2 | Total instructions: {len(instructions)}, Final image")
+    for line in cpu.crtOutput:
+        print("".join(line))
+
 
 
 task1()
